32b21199985fc7b47bba961ae7ef3d699bb9b7b9
Duane Scheepers <98160374+DuaneScheepersWebDeveloper@users.noreply.github.com>
2023-03-15 13:20:42 +0200

    Added code that enables you to render HTML elements
    Added code that enables you to render HTML elements

--- public/app.js
+++ public/app.js
@@ -1,1 +1,24 @@
-"use strict";
+//classes
+import { Invoice } from './model/invoice.js';
+import { Payment } from './model/payment.js';
+import { ListTemplate } from './model/ListTemplate.js';
+const form = document.querySelector('.new-item-form');
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+//List template Instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    console.log(doc);
+    list.render(doc, type.value, 'end');
+});
